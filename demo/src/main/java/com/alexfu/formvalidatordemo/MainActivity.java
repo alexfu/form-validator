7a6b4dda0cb7616383e3920e9c2aabfc60fb1bee
Alex Fu <alex.fu914@gmail.com>
2016-08-12 11:53:53 -0400

    Implement a simple demo

--- demo/src/main/java/com/alexfu/formvalidatordemo/MainActivity.java
+++ demo/src/main/java/com/alexfu/formvalidatordemo/MainActivity.java
@@ -2,12 +2,45 @@ package com.alexfu.formvalidatordemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.alexfu.formvalidator.ValidationResult;
+import com.alexfu.formvalidator.Validator;
+import com.alexfu.formvalidator.rules.EmailRule;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Validator.Callback {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    EditText emailInput = (EditText) findViewById(R.id.email_input);
+
+    Validator validator = new Validator();
+    validator.setCallback(this);
+    validator.addRule(emailInput, new EmailRule("Invalid email."));
+  }
+
+  @Override
+  public void onFieldValidationSuccessful(EditText editText) {
+    editText.setError(null);
+  }
+
+  @Override
+  public void onFieldValidationFailed(EditText editText, List<String> errors) {
+    editText.setError(errors.get(0)); // Show errors one at a time
+  }
+
+  @Override
+  public void onFormValidationSuccessful() {
+    // Do something here when form is fully valid
+  }
+
+  @Override
+  public void onFormValidationFailed(List<ValidationResult> errorValidations) {
+    // Do something here when form is invalid
   }
 }
