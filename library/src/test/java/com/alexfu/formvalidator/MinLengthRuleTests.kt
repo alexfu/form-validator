9b8cf7fc3785128814b21920db8cdec5bac1ef39
Alex Fu <alex.fu914@gmail.com>
2016-08-12 11:21:06 -0400

    New rule tests

---
+++ library/src/test/java/com/alexfu/formvalidator/MinLengthRuleTests.kt
@@ -0,0 +1,23 @@
+package com.alexfu.formvalidator
+
+import org.jetbrains.spek.api.Spek
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class MinLengthRuleTests : Spek({
+
+  describe("min length rule") {
+    val rule = MinLengthRule(5, "Must be at least 5 characters long.")
+
+    it("should return true for good input") {
+      val result = rule.isValid("12345")
+      assertTrue(result)
+    }
+
+    it("should return false for bad input") {
+      val result = rule.isValid("123")
+      assertFalse(result)
+    }
+  }
+
+})
---
+++ library/src/test/java/com/alexfu/formvalidator/RegexRuleTests.kt
@@ -0,0 +1,24 @@
+package com.alexfu.formvalidator
+
+import org.jetbrains.spek.api.Spek
+import java.util.regex.Pattern
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class RegexRuleTests : Spek({
+
+  describe("regex rule") {
+    val rule = RegexRule(Pattern.compile("[0-9]+"), "No match.")
+
+    it("should return true with good input") {
+      val result = rule.isValid("123")
+      assertTrue(result)
+    }
+
+    it("should return true with bad input") {
+      val result = rule.isValid("123ABC")
+      assertFalse(result)
+    }
+  }
+
+})
