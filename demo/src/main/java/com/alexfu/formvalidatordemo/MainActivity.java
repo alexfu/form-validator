f7b325d1a37288f5ba000b9a1c991b6a5e97bf95
Alex Fu <alex.fu914@gmail.com>
2016-12-23 14:19:59 -0500

    Update demo app

--- demo/src/main/java/com/alexfu/formvalidatordemo/MainActivity.java
+++ demo/src/main/java/com/alexfu/formvalidatordemo/MainActivity.java
@@ -2,46 +2,94 @@ package com.alexfu.formvalidatordemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alexfu.formvalidator.DefaultValidationWorker;
-import com.alexfu.formvalidator.ValidationResult;
 import com.alexfu.formvalidator.Validator;
 import com.alexfu.formvalidator.rules.EmailRule;
+import com.alexfu.formvalidator.rules.MinLengthRule;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements DefaultValidationWorker.Callback {
+  private Button validateButton;
+  private EditText firstNameInput, lastNameInput, emailInput;
+  private Validator validator = new Validator(this);
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    EditText emailInput = (EditText) findViewById(R.id.email_input);
-
-    Validator validator = new Validator(this);
-    validator.addRule(emailInput, new EmailRule("Invalid email."));
+    setUpViews();
+    setUpValidator();
   }
 
-  @Override
-  public void onFieldValidationSuccessful(TextView view) {
+  private void setUpValidator() {
+    validator.addRule(firstNameInput, new MinLengthRule(1, "First name cannot be empty."));
+    validator.addRule(lastNameInput, new MinLengthRule(1, "Last name cannot be empty."));
+    validator.addRule(emailInput, new EmailRule("Invalid email."));
+
+    // Form validation
+
+    validateButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        validator.validate();
+      }
+    });
+
+    // Input validation
+
+    validateOnTextChange(firstNameInput, lastNameInput, emailInput);
+  }
+
+  @Override public void onFieldValidationSuccessful(TextView view) {
     view.setError(null);
   }
 
   @Override
   public void onFieldValidationFailed(TextView view, List<String> errors) {
-    view.setError(errors.get(0)); // Show errors one at a time
+    view.setError(errors.get(0));
+  }
+
+  @Override public void onFormValidationSuccessful() {
+    Toast.makeText(this, "Form is valid!", Toast.LENGTH_SHORT).show();
   }
 
   @Override
-  public void onFormValidationSuccessful() {
-    // Do something here when form is fully valid
+  public void onFormValidationFailed() {
+    // Ignore
   }
 
-  @Override
-  public void onFormValidationFailed(List<ValidationResult> errorValidations) {
-    // Do something here when form is invalid
+  private void validateOnTextChange(TextView...inputs) {
+    for (final TextView input : inputs) {
+      input.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+          // Ignore
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+          // Ignore
+        }
+
+        @Override public void afterTextChanged(Editable s) {
+          validator.validate(input);
+        }
+      });
+    }
+  }
+
+  private void setUpViews() {
+    firstNameInput = (EditText) findViewById(R.id.first_name_input);
+    lastNameInput = (EditText) findViewById(R.id.last_name_input);
+    emailInput = (EditText) findViewById(R.id.email_input);
+    validateButton = (Button) findViewById(R.id.button_validate);
   }
 }
