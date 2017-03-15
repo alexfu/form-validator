62caa8006b4de0a7012881cd9da66fff8d617d00
Alex Fu <alex.fu914@gmail.com>
2017-03-15 15:59:25 -0400

    Remove unused interface.

--- library/src/main/java/com/alexfu/formvalidator/ValidationWorker.java
+++
@@ -1,9 +0,0 @@
-package com.alexfu.formvalidator;
-
-/**
- * The actual worker that does the work of validating
- */
-public interface ValidationWorker<T> {
-  T performValidate(ValidationTask[] tasks);
-  T performValidate(ValidationTask task);
-}
